06db776aaf8a29c10224f0c089b0c3be8cfab1b5
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-08-25 20:59:28 +0530

    Remove homelab CA from titanx

--- hosts/titanx/configuration.nix
+++ hosts/titanx/configuration.nix
@@ -28,15 +28,10 @@
   catppuccin.enable = true;
   catppuccin.flavor = "mocha";
 
-  # Homelab CA
-  security.pki.certificateFiles = [
-   ../../gnexus-labs-ca.pem
-  ];
-
   # RKVM stuff
   services.rkvm.enable = true;
   services.rkvm.server.enable = true;
-  services.rkvm.server.settings.password = builtins.readFile ../../private/rkvm_password;
+  services.rkvm.server.settings.password = builtins.readFile ${inputs.private_configs}/rkvm_password;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
