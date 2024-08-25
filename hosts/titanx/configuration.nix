d422793678c1725ae15c2e82c4167953546966af
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-08-25 21:07:33 +0530

    Fix path

--- hosts/titanx/configuration.nix
+++ hosts/titanx/configuration.nix
@@ -31,7 +31,7 @@
   # RKVM stuff
   services.rkvm.enable = true;
   services.rkvm.server.enable = true;
-  services.rkvm.server.settings.password = builtins.readFile ${inputs.private_configs}/rkvm_password;
+  services.rkvm.server.settings.password = builtins.readFile "${inputs.private_configs}/rkvm_password";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
