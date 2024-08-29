44144a35df36922ea0b3a1cc2d6b1fc4f3bbd045
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-08-29 21:04:09 +0530

    Update nvidia settings

--- hosts/titanx/configuration.nix
+++ hosts/titanx/configuration.nix
@@ -58,7 +58,13 @@
 
   hardware = {
   	graphics.enable = true;
-	nvidia.modesetting.enable = true;
+	nvidia = {
+		modesetting.enable = true;
+		powerManagement.enable = false;
+		powerManagement.finegrained = false;
+		open = false;
+		nvidiaSettings = true;
+	};
   };
 
   programs.hyprland = {
