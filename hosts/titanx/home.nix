b14aaef6cc50d723831495e4e00a7716ee6a2ad9
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-08-29 21:05:15 +0530

    Add vscode, wezterm and custom pointer to remove stupid hyprland cursor (nope it dosent remove)

--- hosts/titanx/home.nix
+++ hosts/titanx/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
   # Core home
@@ -13,6 +13,35 @@
   home.packages = with pkgs; [
     hyprshot
   ];
+
+  programs.vscode = {
+  	enable = true;
+	extensions = with pkgs.vscode-extensions; [
+		ms-vscode-remote.remote-ssh
+		teabyii.ayu
+	];
+  };
+
+  programs.wezterm = {
+  	enable = true;
+	package = inputs.wezterm.packages.${pkgs.system}.default;
+  };
+
+  gtk = {
+  	enable = true;
+	cursorTheme = {
+		package = pkgs.catppuccin-cursors.mochaDark;
+		name = "catppuccin-mocha-dark-cursors";
+		size = 24;
+	};
+  };
+
+  home.pointerCursor = {
+  	gtk.enable = true;
+	package = pkgs.catppuccin-cursors.mochaDark;
+	name = "catppuccin-moch-dark";
+	size = 24;
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
