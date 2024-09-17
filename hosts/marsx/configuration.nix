625cc33c939aa0f62ad7e9f36b0da6d52a66c748
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-09-17 13:46:43 +0530

    Add printing support

--- hosts/marsx/configuration.nix
+++ hosts/marsx/configuration.nix
@@ -107,4 +107,13 @@
   	enable = true;
 	xwayland.enable = true;
   };
+
+  services.printing.enable = true;
+  services.avahi = {
+  enable = true;
+  nssmdns4 = true;
+  openFirewall = true;
+  };
+
+  services.printing.drivers = [ pkgs.cnijfilter2 ];
 }
