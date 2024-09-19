79cd27db6f24168f13798a924bc31c095300b49b
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-09-19 13:51:20 +0530

    Update titanx configuration

--- hosts/titanx/configuration.nix
+++ hosts/titanx/configuration.nix
@@ -37,23 +37,39 @@
   services.rkvm.enable = true;
   services.rkvm.server.enable = true;
   services.rkvm.server.settings.password = builtins.readFile "${inputs.private_configs}/rkvm_password";
+  services.rkvm.server.settings.switch-keys = [ "caps-lock" ];
+
+
+  # Note : Remove this when new version of rofi gets released 
+  nixpkgs.overlays = [(final: super: {
+    rofi-wayland-unwrapped = super.rofi-wayland-unwrapped.overrideAttrs({ patches ? [], ... }: {
+      patches = patches ++ [
+        (final.fetchpatch {
+          url = "https://github.com/samueldr/rofi/commit/55425f72ff913eb72f5ba5f5d422b905d87577d0.patch";
+          hash = "sha256-vTUxtJs4SuyPk0PgnGlDIe/GVm/w1qZirEhKdBp4bHI=";
+        })
+      ];
+    });
+  })];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
      waybar
-     hyprpaper
      kitty
      rofi-wayland-unwrapped
      mosh
+     niri
+     swaybg
   ];
 
 
-fonts.fonts = with pkgs; [
-  corefonts
-];
+   fonts.fonts = with pkgs; [
+     corefonts
+  ];
 
   services = {
+  	displayManager.sessionPackages = [ pkgs.niri ]; 
   	openssh.enable = true;
 	xserver = {
 		enable = true;
--- hosts/titanx/home.nix
+++ hosts/titanx/home.nix
@@ -5,15 +5,21 @@
   imports = 
   [
   	../../common/home.nix
+	../../applications/wezterm/wezterm_config.nix
+	../../applications/dunst/dunst_config.nix
   ];
 
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    hyprshot
     pkgs.onlyoffice-bin
   ];
+
+
+  fonts.fontconfig.enable = true;
+  programs.wezterm.enable = true;
+  services.dunst.enable = true;
 
   programs.vscode = {
   	enable = true;
@@ -21,11 +27,6 @@
 		ms-vscode-remote.remote-ssh
 		teabyii.ayu
 	];
-  };
-
-  programs.wezterm = {
-  	enable = true;
-	package = inputs.wezterm.packages.${pkgs.system}.default;
   };
 
   gtk = {
@@ -41,7 +42,6 @@
   	gtk.enable = true;
 	package = pkgs.catppuccin-cursors.mochaDark;
 	name = "catppuccin-moch-dark";
-	size = 24;
   };
 
   # Let Home Manager install and manage itself.
