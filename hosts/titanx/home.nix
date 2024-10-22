c8a34871462aa989cf3045b6d5ca0df09c4f0d0d
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-10-22 21:59:31 +0530

    Add custom wrapper to make onlyoffice work with xwayland-satellite

--- hosts/titanx/home.nix
+++ hosts/titanx/home.nix
@@ -13,7 +13,20 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    pkgs.onlyoffice-bin
+  	# Custom Wrapper to make onlyoffice work with
+	# xwayland-satellite 
+  	(pkgs.symlinkJoin {
+        	name = "onlyoffice-bin";
+        	paths = [ pkgs.onlyoffice-bin ];
+        	buildInputs = [ pkgs.makeWrapper ];
+        	postBuild = ''
+        	wrapProgram $out/bin/onlyoffice-desktopeditors \
+        	--set DISPLAY :1
+		mv $out/share/applications/onlyoffice-desktopeditors.desktop{,.orig}
+		substitute $out/share/applications/onlyoffice-desktopeditors.desktop{.orig,}\
+		--replace-fail Exec=${pkgs.onlyoffice-bin}/bin/onlyoffice-desktopeditors Exec=$out/bin/onlyoffice-desktopeditors 
+        	'';
+  	})
   ];
 
 
