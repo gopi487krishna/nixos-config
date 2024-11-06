bb481e77a5e33e6d1b878af8b867d59183558589
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-11-06 12:41:00 +0530

    Add support for hardware encoding

--- hosts/venusx/configuration.nix
+++ hosts/venusx/configuration.nix
@@ -90,4 +90,18 @@
 	xwayland.enable = true;
   };
 
+  # Open GL options
+  nixpkgs.config.packageOverrides = pkgs: {
+  intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = { # hardware.graphics on unstable
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
+
 }
