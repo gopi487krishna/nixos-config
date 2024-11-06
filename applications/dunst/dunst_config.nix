a5e4d1fbfe4bb0979851e4a157399c24d3f758c4
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-11-06 12:39:55 +0530

    Fix font

--- applications/dunst/dunst_config.nix
+++ applications/dunst/dunst_config.nix
@@ -5,10 +5,11 @@
     settings = {
       global = {
         timeout = 10;
+        font = "JetBrains Mono 12";
         separator_height = 2;
         corner_radius = 5;
-	frame_color = "#ffd60aff";
-	background = "#000814";
+	      frame_color = "#ffd60aff";
+	      background = "#000814";
       };
    };
   };
