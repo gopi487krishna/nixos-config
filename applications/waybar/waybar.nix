9846dab4f50154f8e88b0030ff356dc1aac4d8b0
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-11-08 05:40:47 +0530

    Add sgroup patch for waybar

--- applications/waybar/waybar.nix
+++ applications/waybar/waybar.nix
@@ -5,9 +5,15 @@
       # Patch for timer support
       patches = patches ++ [
         (final.fetchpatch {
-          url = "https://github.com/gopi487krishna/Waybar/commit/3596e10388f03fc2230f805265a1c3dc03f64492.patch";
-          hash = "sha256-ZZYRuGkUZ/7Mbx/TnZBnnnGEjTTDjP9fZn5wtcy7nQI=";
+          url = "https://github.com/gopi487krishna/Waybar/commit/9f0d6d69fbf5941573b9f443f3c4e6d297b969a3.patch";
+          hash = "sha256-ONCRpndcNR18vcrZs0mEhmW7kFWFDRGqQBpUfbNObVA=";
         })
+
+        (final.fetchpatch {
+          url = "https://github.com/gopi487krishna/Waybar/commit/953642a7548ab39721e914f25f486d7ca71a9f52.patch";
+          hash = "sha256-NsM9k6NbWxkxvbIAETexws1jzL0GPUXzZQcltbLwXMM=";
+        })
+
       ];
     });
   })];
