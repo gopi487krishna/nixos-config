e0a7a6d07987609328905ed40b7df5bff691c891
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-11-18 17:05:14 +0530

    Add qemu/ use niri from nixpkgs

--- hosts/venusx/configuration.nix
+++ hosts/venusx/configuration.nix
@@ -73,9 +73,15 @@
      rofi-wayland-unwrapped
      mosh
      wireguard-tools
-     inputs.niri.packages.${system}.default
+     niri
      swaybg
      surface-control
+     qemu
+     (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+     qemu-system-x86_64 \
+        -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+        "$@"
+     '')
   ];
   services.displayManager.sessionPackages = [ pkgs.niri ];
 
