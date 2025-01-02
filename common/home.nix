7f22346c47734bf09883a977a867591f3febf816
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-01-02 09:23:13 +0530

    Switch from wezterm ssh to mosh

--- common/home.nix
+++ common/home.nix
@@ -90,13 +90,16 @@
   # Fish shell support
   programs.fish = {
   shellAliases = {
-    connect_ubwork = "wezterm cli spawn --domain-name SSH:ubuntu-work -- sh -c 'cd /home/cooldev/kbounds_pass; bash -l'"; 
-    connect_galileo = "wezterm cli spawn --domain-name SSH:galileo"; 
+    #connect_ubwork = "wezterm cli spawn --domain-name SSH:ubuntu-work -- sh -c 'cd /home/cooldev/kbounds_pass; bash -l'"; 
+    connect_ubwork = "mosh ubuntu-work";
+    connect_galileo = "mosh galileo";
+    #connect_galileo = "wezterm cli spawn --domain-name SSH:galileo"; 
     dotfiles = "cd /home/cooldev/.dotfiles"; 
   };
   enable = true;
 	interactiveShellInit = ''
 	set fish_greeting
+  fastfetch
 	'';
 	plugins = [
 		{ name = "fzf"; src = pkgs.fishPlugins.fzf-fish.src; }
