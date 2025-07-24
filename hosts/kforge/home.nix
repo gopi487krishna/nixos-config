b2d72d35035410bb2074d525d11e4b320d706908
Gopi Krishna Menon <krishnagopi487@gmail.com>
2025-07-24 22:22:58 +0530

    fix: Add username and state version

--- hosts/kforge/home.nix
+++ hosts/kforge/home.nix
@@ -1,6 +1,26 @@
 {inputs, config, pkgs, ... }:
 
 {
+
+  home.username = "cooldev";
+  home.homeDirectory = "/home/cooldev";
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  nixpkgs = {
+	config = {
+		allowUnfree = true;
+		allowUnfreePredicate = (_: true);
+	};
+  };
+
+
   # Core home
 	#  imports =
 	#  [
