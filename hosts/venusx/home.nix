17d348087c4e8d6ac0ad808a99e65ac52f429e7b
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-07-24 21:43:41 +0530

    feat: Add applications

--- hosts/venusx/home.nix
+++ hosts/venusx/home.nix
@@ -24,6 +24,12 @@
     obsidian
     aider-chat
     zellij
+    watchexec
+    libnotify
+    zoom-us
+    xdg-desktop-portal-gnome
+    xdg-desktop-portal
+    weechat
   ];
 
   fonts.fontconfig.enable = true;
@@ -39,12 +45,33 @@
 	];
   };
 
+  # Zoxide for cd command
+  programs.zoxide.enable = true;
+  programs.zoxide.options = [
+    "--cmd cd"
+  ];
+
   # Enable zellij and provide fish integration
   # programs.zellij = {
   #   enable = true;
-  #   #enableFishIntegration = true;
+  #   enableFishIntegration = true;
+  #   settings = {
+  #     theme = "ayu_dark";
+  #   };
   # };
-  #
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      droidcam-obs
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
