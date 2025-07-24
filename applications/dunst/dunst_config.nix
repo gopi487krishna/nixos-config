2d4b56a76e862d6af2d50c56c547c88a61045ccd
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-07-24 21:42:28 +0530

    refactor: Set font size to 10 instead of 12 for dunst (for keypeek)

--- applications/dunst/dunst_config.nix
+++ applications/dunst/dunst_config.nix
@@ -5,10 +5,12 @@
     settings = {
       global = {
         timeout = 10;
-        font = "JetBrains Mono 12";
+        font = "JetBrainsMono Nerd Font Mono 10";
         separator_height = 2;
         corner_radius = 5;
 	      frame_color = "#ffd60aff";
+        width = "(0,2000)";
+        height = "(0,2000)";
 	      background = "#000814";
       };
    };
