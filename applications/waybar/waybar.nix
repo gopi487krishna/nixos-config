fb315b38124861fee5ca32fbcf2b1092d05a3a5a
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-09-04 05:09:53 +0530

    fix: Remove waybar patches

--- applications/waybar/waybar.nix
+++ applications/waybar/waybar.nix
@@ -2,19 +2,19 @@
 {
   nixpkgs.overlays = [(final: super: {
     waybar = super.waybar.overrideAttrs({ patches ? [], ... } : {
-      patches = patches ++ [
-        # Patch for timer support
-        (final.fetchpatch {
-          url = "https://github.com/gopi487krishna/Waybar/commit/d2aa353e382172d04e444dfe0777339714c9f4ae.patch";
-          hash = "sha256-IC5LhbGbHF0ncbwablI52eJ6+wsvWoXQ9GC4M+bI39E=";
-        })
-
-        (final.fetchpatch {
-          url = "https://github.com/gopi487krishna/Waybar/commit/953642a7548ab39721e914f25f486d7ca71a9f52.patch";
-          hash = "sha256-NsM9k6NbWxkxvbIAETexws1jzL0GPUXzZQcltbLwXMM=";
-        })
-
-      ];
+      # patches = patches ++ [
+      #   # Patch for timer support
+      #   (final.fetchpatch {
+      #     url = "https://github.com/gopi487krishna/Waybar/commit/d2aa353e382172d04e444dfe0777339714c9f4ae.patch";
+      #     hash = "sha256-IC5LhbGbHF0ncbwablI52eJ6+wsvWoXQ9GC4M+bI39E=";
+      #   })
+      #
+      #   (final.fetchpatch {
+      #     url = "https://github.com/gopi487krishna/Waybar/commit/953642a7548ab39721e914f25f486d7ca71a9f52.patch";
+      #     hash = "sha256-NsM9k6NbWxkxvbIAETexws1jzL0GPUXzZQcltbLwXMM=";
+      #   })
+      #
+      # ];
     });
   })];
 }
